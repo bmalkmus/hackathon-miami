5af61bd9e4c080074d1ab9ac94e61b8b609713ec
bmalkmus <bmalkmus23@gmail.com>
2021-01-30 05:39:22 -0800

    contracts done

--- contracts/TagCreation.sol
+++ contracts/TagCreation.sol
@@ -6,6 +6,8 @@ contract TagsDirectory {
     struct Tag {
         uint id;
         address owner;
+        address approved;
+        string name;
     }
 
     struct TagCollection {
@@ -15,6 +17,9 @@ contract TagsDirectory {
     }
 
     event OnCollectionCreation(string name, uint id);
+    event OnTagAddition(string name, uint id, address account);
+    event OnTagApproved(string name, uint id, address from, address to);
+    event OnTagTransfer(string name, uint id, address from, address to);
 
     TagCollection[] public TagCollections;
 
@@ -26,19 +31,71 @@ contract TagsDirectory {
 
     mapping (uint => TagCollection) public Directory;
     mapping (string => Tag[]) public TagsListing;
+    mapping (address => Tag[]) public TagsByOwner;
     // mapping (uint => Tag) public TagNumber;
 
     function getCollectionCount() public view returns (uint){
         return TagCollections.length;
     }
 
+    function getCollectionTagArrayLength(string memory _name) public view returns (uint){
+        return TagsListing[_name].length;
+    }
+
+    function getCollectionTagInfo(string memory _name, uint id) public view returns (uint, address, address, string memory) {
+        Tag memory tag = TagsListing[_name][id-1];
+        return (tag.id, tag.owner, tag.approved, tag.name);
+    }
+
     function CreateCollection (string memory _name) public {
         uint _tagCollectionsID = TagCollections.length + 1;
         TagCollection memory collection = TagCollection(_name, _tagCollectionsID, 1);
         TagCollections.push(collection);
-        Tag memory _tag = Tag( 1, msg.sender );
+        Tag memory _tag = Tag( 1, msg.sender, msg.sender, _name);
         string memory _unique = _name;
         TagsListing[_unique].push(_tag);
+        TagsByOwner[msg.sender].push(_tag);
         emit OnCollectionCreation(_name, _tagCollectionsID);
+    }
+
+    function AddTag (string memory _name, address _account) public {
+        uint _id = TagsListing[_name].length+1;
+        TagsListing[_name].push(Tag(_id, _account, _account, _name));
+        emit OnTagAddition(_name, _id, _account);
+    }
+
+    function ApproveAddress (string memory _name, uint _number, address _newAccount) public {
+        require(msg.sender == TagsListing[_name][_number-1].owner, "You are not the owner");
+        Tag memory temp = TagsListing[_name][_number-1];
+        temp.approved = _newAccount;
+        TagsListing[_name][_number-1] = temp;
+        emit OnTagApproved(_name, _number, msg.sender, _newAccount);
+    }
+
+    function TransferTag (string memory _name, uint _number, address _newAccount) public {
+        require(msg.sender == TagsListing[_name][_number-1].approved || msg.sender == TagsListing[_name][_number-1].owner, "You are not the approved receiver or owner");
+        uint length = TagsListing[_name].length;
+        Tag[] memory Tags = TagsListing[_name];
+        Tag memory owned;
+        Tag memory received;
+        for (uint i=0; i<length; i++){
+            if(Tags[i].owner == msg.sender){
+                owned = Tags[i];
+            }
+            if (Tags[i].owner == _newAccount){
+                received = Tags[i];
+            }
+        }
+        require(owned.id > 0 && received.id > 0);
+        Tag memory swap1 = owned;
+        Tag memory swap2 = received;
+        swap1.owner = _newAccount;
+        swap1.approved = _newAccount;
+        swap2.owner = msg.sender;
+        swap2.approved = msg.sender;
+        TagsListing[_name][owned.id-1] = swap1;
+        TagsListing[_name][received.id-1] = swap2;
+        emit OnTagTransfer(_name, _number, msg.sender, _newAccount);
+
     }
 }
--- test/tagCreation-test.js
+++ test/tagCreation-test.js
@@ -10,4 +10,39 @@ describe("TagsDirectory", function(){
     it("Should have a collection count of 1", async function() {
         expect (await tagsDirectory.getCollectionCount()).to.equal(1);
     })
+    it("Should have a tag array of length 1", async function() {
+        expect (await tagsDirectory.getCollectionTagArrayLength("Sample Collection")).to.equal(1);
+    })
+    it("Should have return an owner address of creator", async function() {
+        const tagInfo = await tagsDirectory.getCollectionTagInfo("Sample Collection", 1);
+        const [owner] = await hre.ethers.getSigners();
+        expect (tagInfo[1]).to.equal(owner.address);
+    })
+    it("Should have a tag length of two", async function (){
+        const [a, AltOwner] = await hre.ethers.getSigners();
+        await tagsDirectory.AddTag("Sample Collection", AltOwner.address);
+        expect (await tagsDirectory.getCollectionTagArrayLength("Sample Collection")).to.equal(2);
+    });
+})
+
+describe("Approve Transfer", function (){
+    let tagsDirectory;
+    beforeEach(async ()=>{
+        const [old, future] = await hre.ethers.getSigners();
+        const TagsDirectory = await hre.ethers.getContractFactory("TagsDirectory");
+        tagsDirectory = await TagsDirectory.deploy();
+        await tagsDirectory.AddTag("Sample Collection", future.address);
+        await tagsDirectory.ApproveAddress("Sample Collection", 1, future.address);
+    })
+    it("Should approve address to be prepped for transfer", async function (){
+        const [old, future] = await hre.ethers.getSigners();
+        const newOwner = await tagsDirectory.getCollectionTagInfo("Sample Collection", 1)
+        expect (newOwner[2]).to.equal(future.address);
+    })
+    it("Transfer Token to new Address", async function (){
+        const [old, future] = await hre.ethers.getSigners();
+        await tagsDirectory.TransferTag("Sample Collection", 1, future.address);
+        const newOwner = await tagsDirectory.getCollectionTagInfo("Sample Collection", 1)
+        expect (newOwner[1]).to.equal(future.address);
+    })
 })
